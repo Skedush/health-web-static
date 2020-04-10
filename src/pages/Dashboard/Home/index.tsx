b950550fbf5639def8ba47e8e51dca515807fa7c
徐力 <xuli@xulideMacBook-Pro.local>
2020-04-10 22:32:29 +0800

    feat(page): home link simple

--- src/pages/Dashboard/Home/index.tsx
+++ src/pages/Dashboard/Home/index.tsx
@@ -130,10 +130,25 @@ class Home extends PureComponent<HomeProps, HomeState> {
     const { entryInfoList } = this.props;
     return (
       <div className={classNames('height100', 'flexColCenter', 'itemCenter', styles.container)}>
-        <div className={styles.link}>
+        <div className={classNames(styles.link, 'flexColCenter')}>
+          <div>精简版</div>
           {entryInfoList.length > 0 &&
             entryInfoList.map(item => (
-              <div key={item.id} className={classNames('flexCenter', 'itemCenter')}>
+              <div key={item.id} className={classNames('flexStart', 'itemCenter')}>
+                <div>{'https://cjsq.net/fillFormOld.html?id=' + item.id}</div>
+                <CopyToClipboard
+                  text={'https://cjsq.net/fillFormOld.html?id=' + item.id}
+                  // text={'http://' + window.location.host + '/#/dashboard/f/' + item.id}
+                  onCopy={this.copySuccess}
+                >
+                  <Button customtype={'master'}>复制链接</Button>
+                </CopyToClipboard>
+              </div>
+            ))}
+          <div>优化版</div>
+          {entryInfoList.length > 0 &&
+            entryInfoList.map(item => (
+              <div key={item.id} className={classNames('flexStart', 'itemCenter')}>
                 <div>{'https://cjsq.net/?id=' + item.id}</div>
                 <CopyToClipboard
                   text={'https://cjsq.net/?id=' + item.id}
