49bb2c05c35b6a515d8fe7818ccf2378e457fcb2
skedush <candycakechocolate@hotmail.com>
2024-07-09 10:36:46 +0800

    fix(config): 访问域名配置修改

--- .umirc.prod.ts
+++ .umirc.prod.ts
@@ -3,9 +3,12 @@ import { IConfig } from 'umi-types';
 
 // ref: https://umijs.org/config/
 const config: IConfig = {
+  // document.ejs中的css需要修改成  .../f/fonts/iconfont.css
+  publicPath: '/f/',
   define: {
-    API_PREFIX: 'https://w.cjsq.net', //集成测试服务器
-    DOMAIN: 'cjsq.net',
+    // API_PREFIX: 'https://w.cjsq.net', //集成测试服务器
+    API_PREFIX: 'https://cjsq.net/api', //集成测试服务器
+    DOMAIN: 'cjsq.net/form',
     // PRE_DOMAIN: 'www.',
     DETAIL_DOMAIN: '323',
   },
--- .umirc.xinjiang.ts
+++ .umirc.xinjiang.ts
@@ -3,6 +3,7 @@ import { IConfig } from 'umi-types';
 
 // ref: https://umijs.org/config/
 const config: IConfig = {
+  // document.ejs中的css需要修改成  .../f/fonts/iconfont.css
   define: {
     API_PREFIX: 'https://hh.jkgl.cc', //集成测试服务器
     DOMAIN: 't.jkgl.cc',
